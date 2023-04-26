6d4c454745ee43d32f5544d13dd5d8b8a64d1fe9
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-04-26 09:57:07 +0100

    added new files

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Image,
+  Dimensions,
+} from 'react-native';
+import { CustomButton } from './components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('./assets/face.jpg')}
+          style={styles.image}
+          resizeMode='contain'
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textHeader}>Janaina Ferreira</Text>
+        <Text style={styles.textBody}>
+          Hello guys, my name is Janaina Ferreira!
+          I'm from Colombia and am really cheerful to be
+          here. Contact me and let's start talking!
+        </Text>
+      </View>
+      <View style={styles.buttonSection}>
+        <CustomButton
+          text={'Ok'}
+          hexBaseColor={'#0000FF'}
+        />
+        <CustomButton
+          text={'Reach out'}
+          hexBaseColor={'#000074'}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +42,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: Dimensions.get('screen').width
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    borderWidth: 2,
+    borderColor: 'black'
+  },
+  textContainer: {
+    flex: 3,
+    backgroundColor: '#00C0CC',
+    width: Dimensions.get('screen').width,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textHeader: {
+    fontFamily: 'serif',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  textBody: {
+    fontFamily: 'serif',
+    fontSize: 20,
+    fontWeight: '400',
+    textAlign: 'justify',
+    margin: 20
+  },
+  buttonSection: {
+    flex: 1,
+    flexDirection: 'row',
+    width: Dimensions.get('screen').width
+  }
 });
---
+++ components/CustomButton.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import {
+    Dimensions,
+    Pressable,
+    StyleSheet,
+    Text
+} from 'react-native';
+
+const CustomButton = ({text, hexBaseColor}) => {
+    const fiftyPerTransparent = hexBaseColor + 'CC'; // 90% transparency.
+    const [backgroundColor, setBackgroundColor] = useState(fiftyPerTransparent);  
+    return (
+        <Pressable
+            style={[styles.buttonContainer, {backgroundColor: backgroundColor}]}
+            onPressIn={() => setBackgroundColor(hexBaseColor)}
+            onPressOut={() => setBackgroundColor(fiftyPerTransparent)}
+        >
+            <Text style={styles.textIn}>{text}</Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+        width: Dimensions.get('screen').width / 2,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textIn: {
+        fontFamily: 'Roboto',
+        fontSize: 18,
+        fontWeight: '500',
+        textAlign: 'center',
+        color: 'white'
+    }
+});
+
+export { CustomButton };
